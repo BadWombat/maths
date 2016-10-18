85b2eb588146ac429a8dcc4f097ee3fc1fcf1343
BadWombat <BadWombat@users.noreply.github.com>
2016-10-18 11:20:28 -0400

    Update slope.py

--- slope.py
+++ slope.py
@@ -1,6 +1,6 @@
 bb = input("what is the y intercept?")
 mm = input("what is the slope?")
-how = input("what is the ending x value?")
+how = input("How many repetitions?")
 yy = bb + (mm * how)
 print yy
 
