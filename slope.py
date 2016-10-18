ca6c73f9edfa5c3099bae804160de3edea99d16c
BadWombat <BadWombat@users.noreply.github.com>
2016-10-18 11:16:31 -0400

    Update slope.py

--- slope.py
+++ slope.py
@@ -2,10 +2,9 @@ bb = input("what is the y intercept?")
 mm = input("what is the slope?")
 how = input("what is the ending x value?")
 yy = mm + (bb * how)
-dmain = input("what is the domian?")
 print yy
 
 while how > 0:
-  how = how - dmain
+  how = how - 1
   yy = mm + (bb * how)
   print yy 
