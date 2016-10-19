adc097d206eba3aad9e8a2b15211fa6e6bc1042d
BadWombat <BadWombat@users.noreply.github.com>
2016-10-19 10:39:41 -0400

    Update slope.py

--- slope.py
+++ slope.py
@@ -1,10 +1,11 @@
 bb = input("what is the y intercept?")
 mm = input("what is the slope?")
 how = input("How many repetitions?")
+dmain = input("what is the domain?")
 yy = bb + (mm * how)
 print yy
 
 while how > 0:
-  how = how - 1
+  how = how - dmain
   yy = bb + (mm * how)
   print yy
